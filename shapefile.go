0ef325cf6925ac71060f6ad4770613e84bfc0488
Jonas <palmuu@gmail.com>
2014-05-07 13:10:36 +0200

    Comments and added DateField for the DBF

--- shapefile.go
+++ shapefile.go
@@ -24,10 +24,13 @@ const (
 	MULTIPATCH            = 31
 )
 
+// Box structure made up from four coordinates. This type
+// is used to represent bounding boxes
 type Box struct {
 	MinX, MinY, MaxX, MaxY float64
 }
 
+// Extends Box with coordinates from box
 func (b *Box) Extend(box Box) {
 	if box.MinX < b.MinX {
 		b.MinX = box.MinX
@@ -67,6 +70,7 @@ func BBoxFromPoints(points []Point) (box Box) {
 	return
 }
 
+// Shape interface
 type Shape interface {
 	BBox() Box
 
@@ -74,9 +78,11 @@ type Shape interface {
 	write(io.Writer)
 }
 
+// Shapefile NULL type
 type Null struct {
 }
 
+// Returns the bounding box of the Null feature
 func (n *Null) BBox() Box {
 	return Box{0.0, 0.0, 0.0, 0.0}
 }
@@ -89,10 +95,12 @@ func (n *Null) write(file io.Writer) {
 	binary.Write(file, binary.LittleEndian, n)
 }
 
+// Shapefile Point type
 type Point struct {
 	X, Y float64
 }
 
+// Returns the bounding box of the Point feature
 func (p *Point) BBox() Box {
 	return Box{p.X, p.Y, p.X, p.Y}
 }
@@ -105,6 +113,7 @@ func (p *Point) write(file io.Writer) {
 	binary.Write(file, binary.LittleEndian, p)
 }
 
+// Shapefile PolyLine type
 type PolyLine struct {
 	Box
 	NumParts  int32
@@ -113,6 +122,7 @@ type PolyLine struct {
 	Points    []Point
 }
 
+// Returns the bounding box of the PolyLine feature
 func (p PolyLine) BBox() Box {
 	return BBoxFromPoints(p.Points)
 }
@@ -135,9 +145,11 @@ func (p *PolyLine) write(file io.Writer) {
 	binary.Write(file, binary.LittleEndian, p.Points)
 }
 
+// Shapefile Polygon type
 // The Polygon structure is identical to the PolyLine structure
 type Polygon PolyLine
 
+// Returns the bounding box of the Polygon feature
 func (p Polygon) BBox() Box {
 	return BBoxFromPoints(p.Points)
 }
@@ -193,10 +205,21 @@ func NumberField(name string, length uint8) Field {
 	return field
 }
 
-// Returns a LogicalField that can be used in SetFields to
-// initialize the DBF file.
+// Returns a FloatField that can be used in SetFields to
+// initialize the DBF file. Used to store floating points
+// with precision in the DBF.
 func FloatField(name string, length uint8, precision uint8) Field {
 	field := Field{Fieldtype: 'F', Size: length, Precision: precision}
+	copy(field.Name[:], []byte(name))
+	return field
+}
+
+// Returns a DateField that can be used in SetFields to
+// initialize the DBF file. Used to store Date strings
+// formatted as YYYYMMDD. Data wise this is the same as
+// a StringField with length 8.
+func DateField(name string) Field {
+	field := Field{Fieldtype: 'D', Size: 8}
 	copy(field.Name[:], []byte(name))
 	return field
 }
