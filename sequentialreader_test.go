3acf081b44f2c9ee1351a10ed9f6f65511027b5e
Fabian Wickborn <fabian.wickborn@diamond-air.at>
2016-06-02 18:19:28 +0200

    First test case for sequential reader

---
+++ sequentialreader_test.go
@@ -0,0 +1,53 @@
+package shp
+
+import (
+	"os"
+	"testing"
+)
+
+func openFile(name string, t *testing.T) *os.File {
+	f, err := os.Open(name)
+	if err != nil {
+		t.Fatalf("Failed to open %s: %v", name, err)
+	}
+	return f
+}
+
+func getShapesSequentially(prefix string, t *testing.T) (shapes []Shape) {
+	shp := openFile(prefix+".shp", t)
+	shx := openFile(prefix+".shx", t)
+	dbf := openFile(prefix+".dbf", t)
+
+	sr := SequentialReaderFromExt(shp, shx, dbf)
+	for sr.Next() {
+		_, shape := sr.Shape()
+		shapes = append(shapes, shape)
+	}
+	if err := sr.Err(); err != nil {
+		t.Fatalf("Error when iterating over the shapes: %v", err)
+	}
+
+	if err := sr.Close(); err != nil {
+		t.Fatalf("Could not close sequential reader: %v", err)
+	}
+	return shapes
+}
+
+func TestSequentialReadPoint(t *testing.T) {
+	prefix := "test_files/point"
+	points := dataForReadTests[prefix]
+
+	shapes := getShapesSequentially(prefix, t)
+	if len(shapes) != len(points) {
+		t.Error("Number of shapes read was wrong.")
+	}
+	for n, s := range shapes {
+		p, ok := s.(*Point)
+		if !ok {
+			t.Fatal("Failed to type assert.")
+		}
+		if !pointsEqual([]float64{p.X, p.Y}, points[n]) {
+			t.Error("Points did not match.")
+		}
+	}
+}
