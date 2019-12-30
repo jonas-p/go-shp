8a532fe84e8de923f904923242425797baeb35c6
Cyrille MEICHEL <cyrille.meichel@treezor.com>
2019-12-30 10:56:01 +0100

    re-enforce zip read

--- zipreader.go
+++ zipreader.go
@@ -8,6 +8,10 @@ import (
 	"strings"
 )
 
+type zipShapeFileSet map[string]*zip.File
+
+type zipFileSet map[string]zipShapeFileSet
+
 // ZipReader provides an interface for reading Shapefiles that are compressed in a ZIP archive.
 type ZipReader struct {
 	sr SequentialReader
@@ -32,26 +36,38 @@ func OpenZip(zipFilePath string) (*ZipReader, error) {
 	if err != nil {
 		return nil, err
 	}
+	return OpenZipFromReadCloser(z)
+}
+
+// OpenZipFromReadCloser opens a ZIP shapes from a zip ReadCloser.
+func OpenZipFromReadCloser(z *zip.ReadCloser) (*ZipReader, error) {
 	zr := &ZipReader{
 		z: z,
 	}
+
 	shapeFiles := shapesInZip(z)
-	if len(shapeFiles) == 0 {
-		return nil, fmt.Errorf("archive does not contain a .shp file")
-	}
-	if len(shapeFiles) > 1 {
-		return nil, fmt.Errorf("archive does contain multiple .shp files")
+
+	if shapeFiles.countExt(".shp") > 1 {
+		return nil, ErrMultipleShpFileInZip
 	}
 
-	shp, err := openFromZIP(zr.z, shapeFiles[0].Name)
-	if err != nil {
-		return nil, err
+	for _, set := range shapeFiles {
+		if set.hasExt(".shp") {
+			shp, err := openFromZIP(zr.z, set[".shp"].Name)
+			if err != nil {
+				return nil, err
+			}
+			// dbf is optional, so no error checking here
+			dbfFile, ok := set[".dbf"]
+			if ok {
+				dbf, _ := openFromZIP(zr.z, dbfFile.Name)
+				zr.sr = SequentialReaderFromExt(shp, dbf)
+			}
+			return zr, nil
+		}
 	}
-	withoutExt := strings.TrimSuffix(shapeFiles[0].Name, ".shp")
-	// dbf is optional, so no error checking here
-	dbf, _ := openFromZIP(zr.z, withoutExt+".dbf")
-	zr.sr = SequentialReaderFromExt(shp, dbf)
-	return zr, nil
+
+	return nil, ErrNoShpFileInZip
 }
 
 // ShapesInZip returns a string-slice with the names (i.e. relatives paths in
@@ -62,21 +78,54 @@ func ShapesInZip(zipFilePath string) ([]string, error) {
 	if err != nil {
 		return nil, err
 	}
-	shapeFiles := shapesInZip(z)
-	for i := range shapeFiles {
-		names = append(names, shapeFiles[i].Name)
+
+	for name, set := range shapesInZip(z) {
+		if set.hasExt(".shp") {
+			names = append(names, name+".shp")
+		}
 	}
 	return names, nil
 }
 
-func shapesInZip(z *zip.ReadCloser) []*zip.File {
-	var shapeFiles []*zip.File
+// shapesInZip build a map of files in the ZIP file:
+// "/hello/world": {
+//     ".shp": File
+//     ".dbf": File
+// }
+func shapesInZip(z *zip.ReadCloser) zipFileSet {
+	result := zipFileSet{}
 	for _, f := range z.File {
-		if strings.HasSuffix(f.Name, ".shp") {
-			shapeFiles = append(shapeFiles, f)
+		ext := path.Ext(f.Name)
+		filename := f.Name[:len(ext)]
+
+		if _, ok := result[filename]; !ok {
+			result[filename] = zipShapeFileSet{}
+		}
+
+		result[filename][strings.ToLower(ext)] = f
+	}
+	return result
+}
+
+// hasExt detect an extention in a part of zip file map
+func (z zipShapeFileSet) hasExt(ext string) bool {
+	for i := range z {
+		if i == ext {
+			return true
 		}
 	}
-	return shapeFiles
+	return false
+}
+
+// countExt counts the number of file with `ext` in a zip file
+func (z zipFileSet) countExt(ext string) int {
+	counter := 0
+	for _, set := range z {
+		if set.hasExt(ext) {
+			counter++
+		}
+	}
+	return counter
 }
 
 // OpenShapeFromZip opens a shape file that is contained in a ZIP archive. The
